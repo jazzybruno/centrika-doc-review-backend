7313a6e69e3404249b1f1f50f3c213cb53a1ae36
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-10-30 21:35:49 +0200

    [Add] Added the notification model

---
+++ src/main/java/rw/ac/rca/centrika/controllers/HistoryController.java
@@ -0,0 +1,55 @@
+package rw.ac.rca.centrika.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rw.ac.rca.centrika.dtos.requests.CreateHistoryDTO;
+import rw.ac.rca.centrika.dtos.requests.UpdateHistoryDTO;
+import rw.ac.rca.centrika.models.History;
+import rw.ac.rca.centrika.services.HistoryService;
+import rw.ac.rca.centrika.utils.ApiResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/history")
+public class HistoryController {
+
+    private final HistoryService historyService;
+
+    @Autowired
+    public HistoryController(HistoryService historyService) {
+        this.historyService = historyService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity getAllHistory() {
+        List<History> histories = historyService.getAllHistory();
+        return ResponseEntity.ok(ApiResponse.success(histories));
+    }
+
+    @GetMapping("/id/{historyId}")
+    public ResponseEntity getHistoryById(@PathVariable UUID historyId) {
+        History history = historyService.getHistoryById(historyId);
+        return ResponseEntity.ok(ApiResponse.success(history));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity createHistory(@RequestBody CreateHistoryDTO createHistoryDTO) {
+        History history = historyService.createHistory(createHistoryDTO);
+        return ResponseEntity.ok(ApiResponse.success(history));
+    }
+
+    @PutMapping("/update/{historyId}")
+    public ResponseEntity updateHistory(@PathVariable UUID historyId, @RequestBody UpdateHistoryDTO updateHistoryDTO) {
+        History history = historyService.updateHistory(historyId, updateHistoryDTO);
+        return ResponseEntity.ok(ApiResponse.success(history));
+    }
+
+    @DeleteMapping("/delete/{historyId}")
+    public ResponseEntity deleteHistory(@PathVariable UUID historyId) {
+        History history = historyService.deleteHistory(historyId);
+        return ResponseEntity.ok(ApiResponse.success(history));
+    }
+}
---
+++ src/main/java/rw/ac/rca/centrika/models/Notification.java
@@ -0,0 +1,25 @@
+package rw.ac.rca.centrika.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID notId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private String message;
+    private boolean isRead;
+}
