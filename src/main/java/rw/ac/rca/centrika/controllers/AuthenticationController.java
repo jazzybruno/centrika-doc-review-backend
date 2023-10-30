c449ca8b7a79f0bcd19cff0a36b6f286f9a861ac
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-10-31 00:58:46 +0200

    [Add] added then other auth methods to be implemented

--- src/main/java/rw/ac/rca/centrika/controllers/AuthenticationController.java
+++ src/main/java/rw/ac/rca/centrika/controllers/AuthenticationController.java
@@ -7,10 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rw.ac.rca.centrika.dtos.requests.SignInDTO;
 import rw.ac.rca.centrika.security.JwtTokenProvider;
 import rw.ac.rca.centrika.utils.ApiResponse;
@@ -40,5 +37,33 @@ public class AuthenticationController {
             e.printStackTrace();
         }
         return ResponseEntity.ok(ApiResponse.success(new JWTAuthenticationResponse(jwt)));
+    }
+
+    @PostMapping("/initiate-password-reset")
+    public ResponseEntity<ApiResponse> initiatePasswordReset(@RequestParam String email) {
+        // Check if the email exists in your user database
+        // Generate a unique token for password reset
+        // Save the token in the database along with the user's email and an expiration timestamp
+        // Send an email to the user with the reset link (containing the token)
+        // Return a success response
+        return ResponseEntity.ok(ApiResponse.success("Password reset initiated successfully. Check your email for instructions."));
+    }
+
+    @GetMapping("/verify-email")
+    public ResponseEntity<ApiResponse> verifyEmail(@RequestParam String token) {
+        // Check if the token is valid and not expired
+        // Mark the user's email as verified in the database
+        // Delete or invalidate the token to prevent further use
+        // Return a success response
+        return ResponseEntity.ok(ApiResponse.success("Email verified successfully."));
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<ApiResponse> resetPassword(@RequestParam String token, @RequestParam String newPassword) {
+        // Check if the token is valid and not expired
+        // Update the user's password in the database
+        // Delete or invalidate the token to prevent further use
+        // Return a success response
+        return ResponseEntity.ok(ApiResponse.success("Password reset successful."));
     }
 }
