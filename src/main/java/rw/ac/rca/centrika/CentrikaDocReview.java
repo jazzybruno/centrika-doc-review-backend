ed7a94f4ff7812bc52bf112183764fcdf28812b5
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-11-02 23:31:09 +0200

    [Add]Added cors in the application

--- src/main/java/rw/ac/rca/centrika/CentrikaDocReview.java
+++ src/main/java/rw/ac/rca/centrika/CentrikaDocReview.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import rw.ac.rca.centrika.enumerations.EDocStatus;
 import rw.ac.rca.centrika.enumerations.EUserRole;
 import rw.ac.rca.centrika.repositories.IRoleRepository;
@@ -24,6 +26,19 @@ public class CentrikaDocReview {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CentrikaDocReview.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// Allow all origins and methods
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*");
+			}
+		};
 	}
 
 	@Bean
