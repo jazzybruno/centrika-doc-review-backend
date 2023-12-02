201062f58842db66cb6fafa8ac55ec9f9e6a65c5
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-02 20:32:47 +0200

    [Add] Added the department head service implementation

--- src/main/java/rw/ac/rca/centrika/repositories/IDepartmentHeadRepository.java
+++ src/main/java/rw/ac/rca/centrika/repositories/IDepartmentHeadRepository.java
@@ -1,9 +1,14 @@
 package rw.ac.rca.centrika.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import rw.ac.rca.centrika.models.Department;
 import rw.ac.rca.centrika.models.DepartmentHead;
+import rw.ac.rca.centrika.models.User;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IDepartmentHeadRepository extends JpaRepository<DepartmentHead, UUID> {
+    DepartmentHead findDepartmentHeadByDepartmentId(Department department);
+    DepartmentHead findDepartmentHeadByUserId(User user);
 }
--- src/main/java/rw/ac/rca/centrika/services/serviceImpl/DepartmentHeadServiceImpl.java
+++ src/main/java/rw/ac/rca/centrika/services/serviceImpl/DepartmentHeadServiceImpl.java
@@ -1,45 +1,112 @@
 package rw.ac.rca.centrika.services.serviceImpl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import rw.ac.rca.centrika.dtos.CreateDepartmentHeadDTO;
+import rw.ac.rca.centrika.exceptions.InternalServerErrorException;
+import rw.ac.rca.centrika.exceptions.NotFoundException;
+import rw.ac.rca.centrika.models.Department;
 import rw.ac.rca.centrika.models.DepartmentHead;
+import rw.ac.rca.centrika.models.User;
+import rw.ac.rca.centrika.repositories.IDepartmentHeadRepository;
+import rw.ac.rca.centrika.repositories.IDepartmentRepository;
+import rw.ac.rca.centrika.repositories.IUserRepository;
 import rw.ac.rca.centrika.services.DepartmentHeadService;
 
 import java.util.List;
 import java.util.UUID;
 
+@RequiredArgsConstructor
+@Service
 public class DepartmentHeadServiceImpl implements DepartmentHeadService {
+
+    private IDepartmentHeadRepository departmentHeadRepository;
+    private IUserRepository userRepository;
+    private IDepartmentRepository departmentRepository;
+    private DepartmentHead departmentHead;
+    private List<DepartmentHead> departmentHeads;
+
+    public DepartmentHeadServiceImpl(IDepartmentHeadRepository departmentHeadRepository, IUserRepository userRepository, IDepartmentRepository departmentRepository) {
+        this.departmentHeadRepository = departmentHeadRepository;
+        this.userRepository = userRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
     @Override
     public List<DepartmentHead> getAllDepartmentHeads() {
-        return null;
+        try {
+            return departmentHeadRepository.findAll();
+        }catch (Exception e){
+            throw new InternalServerErrorException("Error while getting all department heads");
+        }
     }
 
     @Override
     public DepartmentHead getDepartmentHeadById(UUID departmentHeadId) {
-        return null;
+        try {
+            return departmentHeadRepository.findById(departmentHeadId).orElseThrow(() -> new InternalServerErrorException("Department head not found"));
+        }catch (Exception e){
+            throw new InternalServerErrorException("Error while getting department head by id");
+        }
     }
 
     @Override
     public DepartmentHead createDepartmentHead(CreateDepartmentHeadDTO departmentHeadDTO) {
-        return null;
+        Department department = departmentRepository.findById(departmentHeadDTO.getDepartmentId()).orElseThrow(() -> new NotFoundException("Department not found"));
+        User user = userRepository.findById(departmentHeadDTO.getUserId()).orElseThrow(() -> new NotFoundException("User not found"));
+        departmentHead = new DepartmentHead();
+        departmentHead.setDepartmentId(department);
+        departmentHead.setUserId(user);
+        try {
+            return departmentHeadRepository.save(departmentHead);
+        }catch (Exception e){
+            throw new InternalServerErrorException("Error while creating department head");
+        }
     }
 
     @Override
+    @Transactional
     public DepartmentHead updateDepartmentHead(UUID departmentHeadId, CreateDepartmentHeadDTO departmentHeadDTO) {
-        return null;
+        departmentHead = getDepartmentHeadById(departmentHeadId);
+        try {
+        Department department = departmentRepository.findById(departmentHeadDTO.getDepartmentId()).orElseThrow(() -> new NotFoundException("Department not found"));
+        User user = userRepository.findById(departmentHeadDTO.getUserId()).orElseThrow(() -> new NotFoundException("User not found"));
+        departmentHead.setDepartmentId(department);
+        departmentHead.setUserId(user);
+        return departmentHead;
+        }catch (Exception e){
+            throw new InternalServerErrorException("Error while updating department head");
+        }
     }
 
     @Override
     public DepartmentHead deleteDepartmentHead(UUID departmentHeadId) {
-        return null;
+        departmentHead = getDepartmentHeadById(departmentHeadId);
+        try {
+            departmentHeadRepository.delete(departmentHead);
+            return departmentHead;
+        }catch (Exception e){
+            throw new InternalServerErrorException("Error while deleting department head");
+        }
     }
 
     @Override
     public DepartmentHead getAllDepartmentHeadByDepartmentId(UUID departmentId) {
-        return null;
+        Department department = departmentRepository.findById(departmentId).orElseThrow(() -> new NotFoundException("Department not found"));
+        try {
+            return departmentHeadRepository.findDepartmentHeadByDepartmentId(department);
+        }catch (Exception e){
+            throw new InternalServerErrorException("Error while getting department head by department id");
+        }
     }
 
     @Override
     public DepartmentHead getDepartmentHeadByUserId(UUID userId) {
-        return null;
+        try {
+            return departmentHeadRepository.findDepartmentHeadByUserId(userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found")));
+        }catch (Exception e){
+            throw new InternalServerErrorException("Error while getting department head by user id");
+        }
     }
 }
