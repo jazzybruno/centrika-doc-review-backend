ee33adf95be7cf8972ad4e2f2eec479ee2c13e91
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-02 12:29:11 +0200

    [Add] Revamped the comment service implementor

--- src/main/java/rw/ac/rca/centrika/repositories/ICommentRepository.java
+++ src/main/java/rw/ac/rca/centrika/repositories/ICommentRepository.java
@@ -1,15 +1,13 @@
 package rw.ac.rca.centrika.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import rw.ac.rca.centrika.models.Comment;
-import rw.ac.rca.centrika.models.Document;
-import rw.ac.rca.centrika.models.DocumentReview;
-import rw.ac.rca.centrika.models.User;
+import rw.ac.rca.centrika.models.*;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ICommentRepository extends JpaRepository<Comment, UUID> {
     public List<Comment > findAllByCommentCreator(User user);
+    public List<Comment> findAllByReviewAction(ReviewAction reviewAction);
 
 }
