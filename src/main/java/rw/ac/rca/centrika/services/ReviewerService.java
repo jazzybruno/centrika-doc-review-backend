5a2ddbda02b229710e51aac47c39d7bdd836648a
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-02 11:39:37 +0200

    [Add] Added the reviwer service

--- src/main/java/rw/ac/rca/centrika/services/ReviewerService.java
+++ src/main/java/rw/ac/rca/centrika/services/ReviewerService.java
@@ -1,4 +1,22 @@
 package rw.ac.rca.centrika.services;
 
+import rw.ac.rca.centrika.dtos.CreateReviewerDTO;
+import rw.ac.rca.centrika.dtos.requests.UpdateUserDepartmentDTO;
+import rw.ac.rca.centrika.models.Reviewer;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ReviewerService {
+    // CRUD methods here
+     public List<Reviewer> findAll();
+        public Reviewer findById(UUID reviewerId);
+        public Reviewer createReviewer(CreateReviewerDTO createReviewerDTO);
+        public Reviewer updateReviewer(UUID reviewerId, CreateReviewerDTO updateReviewerDTO);
+        public Boolean deleteReviewer(UUID reviewerId);
+
+    // Custom methods here
+
+        public List<Reviewer> findByDocumentReviewId(UUID documentReviewId);
+
 }
