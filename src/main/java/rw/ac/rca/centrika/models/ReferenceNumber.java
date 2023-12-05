d6222d915dc14669f3bc76d43601f53cd50904ad
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-05 14:27:38 +0200

    [Add] Added the reference number generation

--- src/main/java/rw/ac/rca/centrika/models/ReferenceNumber.java
+++ src/main/java/rw/ac/rca/centrika/models/ReferenceNumber.java
@@ -17,17 +17,25 @@ import java.util.UUID;
 @Getter
 @Setter
 public class ReferenceNumber {
+
+    // key identification
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    // details of the reference number
     private String referenceNumber;
     private String title;
     private String destination;
-    private Date createdAt;
     @Enumerated(EnumType.STRING)
     private ERefNumStatus status;
+
+    // details of the expiration and creation
+    private Date createdAt;
     private Date updatedAt;
     private Date deletedAt;
+
+    // relationships
     @ManyToOne
     @JoinColumn(name = "created_by")
     private User createdBy;
--- src/main/java/rw/ac/rca/centrika/repositories/IReferenceNumberRepository.java
+++ src/main/java/rw/ac/rca/centrika/repositories/IReferenceNumberRepository.java
@@ -1,9 +1,20 @@
 package rw.ac.rca.centrika.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import rw.ac.rca.centrika.enumerations.ERefNumStatus;
+import rw.ac.rca.centrika.models.Department;
 import rw.ac.rca.centrika.models.ReferenceNumber;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IReferenceNumberRepository extends JpaRepository<ReferenceNumber, UUID> {
+    @Query("SELECT r FROM ReferenceNumber r WHERE r.createdBy.department = ?1")
+    List<ReferenceNumber> findAllByDepartment(Department department);
+
+    @Query("SELECT r FROM ReferenceNumber r WHERE r.destination like %?1%")
+    List<ReferenceNumber> searchByDestination(String destination);
+
+    List<ReferenceNumber> findAllByStatus(ERefNumStatus status);
 }
