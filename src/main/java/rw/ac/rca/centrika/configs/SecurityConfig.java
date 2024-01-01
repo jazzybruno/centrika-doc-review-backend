b89c2a3741cc94062ec7fc42c134a9168fc52f94
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2024-01-01 08:36:41 +0200

    [Add] New Feature in config

--- src/main/java/rw/ac/rca/centrika/configs/SecurityConfig.java
+++ src/main/java/rw/ac/rca/centrika/configs/SecurityConfig.java
@@ -106,17 +106,17 @@ public class SecurityConfig {
 		};
 	}
 
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-		configuration.setExposedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+//	@Bean
+//	CorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration configuration = new CorsConfiguration();
+//		configuration.setAllowedOrigins(Arrays.asList("*"));
+//		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+//		configuration.setExposedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", configuration);
+//		return source;
+//	}
 
 
 	@Bean
